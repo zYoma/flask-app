38d76c18343f40470c42a899d9437f119afdf753
zYoma <zyoma@mail.ru>
2020-08-17 14:36:11 +0300

    доке7

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, 'У меня получилось!')
+        self.assertEqual(r.data.decode(), 'У меня получилось!')
 
     def test_post(self):
         r = self.app.post('/')
