b67c0fbb2fde88caf18136d40f1e2ecfc5d9bb7e
zYoma <zyoma@mail.ru>
2020-08-17 14:16:10 +0300

    докер

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+import hello as tested_app
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
+
+    def test_get(self):
+        r = self.app.get('/')
+        self.assertEqual(r.data, 'У меня получилось!')
+
+    def test_post(self):
+        r = self.app.post('/')
+        self.assertEqual(r.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
